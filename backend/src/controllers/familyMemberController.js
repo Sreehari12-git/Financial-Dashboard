b2d02e3a4488c59323dd3a70f0051727482d1ddf
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-05-18 15:25:03 +0530

    Added get family member

---
+++ backend/src/controllers/familyMemberController.js
@@ -0,0 +1,30 @@
+import prisma from "../config/prisma";
+
+export const familyMemberController = async(req,res) => {
+    try {
+        const userId = req.user.id;
+
+        if(!userId) {
+            return res.status(401).json({
+                message: "User not found"
+            })
+        }
+
+        const familyMembers = await prisma.familyMember.findMany({
+            where: {
+                userId: userId
+            }
+        })
+
+        return res.status(200).json({
+            message: "Family members fetched successfully",
+            data: familyMembers
+        })
+    }
+    catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
